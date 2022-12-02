72988c7b6e9438c77ade053967243f6f07f60ef1
RedGlimmer <107247457+RedGlimmer@users.noreply.github.com>
2022-12-02 17:46:55 +0100

    update

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 import tkinter as tk
 from sudoku import Sudoku
 
+
 root = tk.Tk()
-
 root.title("Sudoku")
+root.minsize(600, 700)
 
-frame = tk.Frame(root)
-frame.place(relwidth = 1, relheight = 1)
+class UI:
+
+    def __init__(self, master):
+        
+        self.canvas = tk.Canvas(master)
+        self.canvas.place(relwidth = 1, relheight = 1)
+
+        self.button = []
+        for i in range(9):
+            self.button.append([])
+            for j in range(9):
+                self.button[i].append(None)
+
+    def edit_box():
+        pass
+
+    def make_box(self, locationX, locationY):
+        self.button[locationX][locationY] = tk.Button(self.canvas)
+        self.button[locationX][locationY].place(height = 50, width = 50,
+                                                relx = 0.05 + 0.1 * locationX, rely = 0.1 * locationY)
+
+interface = UI(root)
+for i in range(9):
+    for j in range(9):
+        interface.make_box(i, j)
+
+
+sudoku1 = Sudoku()
+sudoku1.enter_value(5, 0, 0)
+print(sudoku1.get_value(0, 0))
+#sudoku1.find_value(1, 1)
+print(sudoku1.get_value(1, 1))
+
 root.mainloop()
--- sudoku.py
+++ sudoku.py
@@ -1,2 +1,33 @@
+import random
+
 class Sudoku:
-    pass
+    
+    def __init__(self):
+        self.value = []
+        self.valueCorrect = []
+        for i in range(9):
+            self.value.append([])
+            self.valueCorrect.append([])
+            for j in range(9):
+                self.value[i].append(None)
+                self.valueCorrect[i].append(None)
+
+    def get_value(self, locationX, locationY):
+        return(self.value[locationX][locationY])
+
+    def get_correct_value(self, locationX, locationY):
+        pass
+
+    def enter_value(self, value, locationX, locationY):
+        assert type(value) == int, "Value has to be an integer!"
+        assert value >= 0 and value <= 9, "Value has to be in range 0 - 9!"
+
+        self.value[locationX][locationY] = value
+
+    def find_values(self, locationX, locationY):
+        for i in range(9):
+            self.valueCorrect[locationX][locationY]
+
+    def generate_values(self, locationX, locationY):
+        self.value[locationX][locationY] = random.randrange(0, 9, 1)
+
